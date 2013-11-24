2be92dc5af9256e81a068d302d9eb8158bfd21e5
Marco Pivetta <ocramius@gmail.com>
2013-11-24 20:57:56 +0100

    Introducing base module tests

---
+++ tests/OcraServiceManagerTest/ModuleTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace OcraServiceManagerTest;
+
+use OcraServiceManager\Module;
+use PHPUnit_Framework_TestCase;
+use Zend\ServiceManager\ServiceManager as BaseServiceManager;
+use Zend\Mvc\Service\ServiceManagerConfig;
+
+/**
+ * @author  Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ */
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \OcraServiceManager\Module::getConfig
+     */
+    public function testGetConfig()
+    {
+        $module = new Module();
+        $config = $module->getConfig();
+
+        $this->assertInternalType('array', $config);
+        $this->assertTrue(isset($config['ocra_service_manager']['logged_service_manager']));
+        $this->assertSame($config, unserialize(serialize($config)), 'Config is serializable');
+    }
+
+    /**
+     * @covers \OcraServiceManager\Module::getConfig
+     */
+    public function testGetServiceConfig()
+    {
+        $module = new Module();
+        $config = $module->getServiceConfig();
+
+        $this->assertInternalType('array', $config);
+    }
+}
